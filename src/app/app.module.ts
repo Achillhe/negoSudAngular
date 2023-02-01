82793fb95bf4f9f7057e5c923168afc4e45fd835
Antoine Petit <antoine.petit4723@gmail>
2023-02-01 10:57:48 +0100

    rajout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientComponent } from './components/client/client/client.component';
 import { ProduitComponent } from './components/produit/produit/produit.component';
+import { ConnexionComponent } from './components/connexion/connexion/connexion.component';
+import { CommandeComponent } from './components/commande/commande/commande.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientComponent,
-    ProduitComponent
+    ProduitComponent,
+    ConnexionComponent,
+    CommandeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/client/client/client.component.ts
+++ src/app/components/client/client/client.component.ts
@@ -8,3 +8,5 @@ import { Component } from '@angular/core';
 export class ClientComponent {
   title = "test";
 }
+
+
